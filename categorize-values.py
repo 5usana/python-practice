62cc8041e155ac2b854bcba974600dbdc5e33f0f
Susana Martins <74124144+5usana@users.noreply.github.com>
2022-08-22 15:21:41 -0700

    Create categorize-values.py
    use data types: numeric, string, list.  use for loop, + print()

---
+++ categorize-values.py
@@ -0,0 +1,14 @@
+myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
+    
+# susanasList = [37, True, 1.02, "I love mangos.", "7"]
+
+# print(susanasList)
+
+# for item in susanasList:
+#     print("{} is of data type {}".format(item, type(item)))
+
+# for item in myMixedTypeList:
+#     print("{}".format(item))
